5dc22551fe1f6b074a16eea0f0d4cd33c2f9d19d
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-02-08 16:05:28 -0300

    Inserindo as imagens dos lanches, titulos, descrições e também o scroll para direcionar a categoria selecionada

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,29 +1,58 @@
-import {useState} from "react"
-import { View, FlatList } from "react-native"
-import{CATEGORIES} from "@/utils/data/products"
-import { CategoryButton } from "@/components/category_button"
+import { useState, useRef } from "react"
+import { View, FlatList, SectionList, Text } from "react-native"
+import { CATEGORIES, MENU } from "@/utils/data/products"
 import { Header } from "@/components/header"
+import { Product } from "@/components/product"
+import { CategoryButton } from "@/components/category_button"
 
 export default function Home() {
     const [category, setCategory] = useState(CATEGORIES[0])
 
-    function handleCategorySelect(selectedCategory: string){
+    const sectionListRef = useRef<SectionList>(null)
+
+    function handleCategorySelect(selectedCategory: string) {
         setCategory(selectedCategory)
+
+        const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+
+        if(sectionListRef.current){
+            sectionListRef.current.scrollToLocation({
+                animated: true,
+                sectionIndex,
+                itemIndex: 0,
+            })
+        }
     }
 
     return (
         <View className="flex-1 pt-8">
             <Header title="Faça o seu pedido" cardQuantityItems={5} />
             <FlatList
-            data={CATEGORIES}
-            keyExtractor={(item) => item}
-            renderItem={({item}) => (
-            <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)}/>
-            )}
-            horizontal
-            className="max-h-10 mt-5"
-            showsHorizontalScrollIndicator={false}
-            contentContainerStyle={{gap:12, paddingHorizontal:20}}
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)} />
+                )}
+                horizontal
+                className="max-h-10 mt-5"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+            />
+
+            <SectionList
+                ref={sectionListRef}
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false}
+                renderItem={({ item }) => <Product data={item} />}
+                renderSectionHeader={({ section: { title } }) => (
+                    <Text className="text-xl text-white font-heading mt-8 mb-3">
+                        {title}
+                    </Text>
+                )}
+                className="flex-1 p-5"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
             />
         </View>
     )
